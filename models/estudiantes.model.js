3e276c43ab40df7827c3c465d9b07209357f7224
Aron <aroon.suy@gmail.com>
2024-05-07 19:18:20 -0400

    se actualiza los errores y mensajes dentro de model.js

--- models/estudiantes.model.js
+++ models/estudiantes.model.js
@@ -2,35 +2,44 @@ import { pool } from "../database/connection.js"
 
 
 const all = async() => {
-   const {rows} = await pool.query("SELECT * FROM estudiantes")
-   return rows
+    try {
+        const querySQL = 'SELECT * FROM estudiantes';
+        const result = await pool.query({ text: querySQL });
+        console.log('Estudiantes registrados:', result.rows);
+    } catch (error) {
+        console.error('Error al consultar estudiantes:', error.message);
+    }
 }
 
 const create = async (estudiantes) => {
-    const querySQL = 'INSERT INTO estudiantes (nombre, rut, curso, nivel) VALUES ($1, $2, $3, $4) RETURNING *';
-    const { rows } = await pool.query(querySQL, [estudiantes.nombre, estudiantes.rut, estudiantes.curso, estudiantes.nivel]);
-    return rows[0]; // Devolver la primera fila (el estudiante recién insertado)
+    try {
+        const querySQL = 'INSERT INTO estudiantes (nombre, rut, curso, nivel) VALUES ($1, $2, $3, $4)';
+        const values = [estudiante.nombre, estudiante.rut, estudiante.curso, estudiante.nivel];
+        const result = await pool.query({ text: querySQL, values });
+        console.log('Estudiante agregado correctamente:', result.rows[0]);
+    } catch (error) {
+        console.error('Error al agregar estudiante:', error.message);
+    }
 };
 
 const update = async (estudiantes) => {
     try {
-        const querySQL = 'UPDATE estudiantes SET nombre = $1, curso = $2, nivel = $3 WHERE rut = $4 RETURNING *';
-        const { rows } = await pool.query(querySQL, [estudiantes.nombre, estudiantes.curso, estudiantes.nivel, estudiantes.rut]);
-        return rows;
+        const querySQL = 'UPDATE estudiantes SET nombre = $1, curso = $2, nivel = $3 WHERE rut = $4';
+        const values = [estudiante.nombre, estudiante.curso, estudiante.nivel, estudiante.rut];
+        const result = await pool.query({ text: querySQL, values });
+        console.log('Estudiante actualizado correctamente:', result.rows[0]);
     } catch (error) {
-        // Manejar errores aquí si es necesario
-        throw error;
+        console.error('Error al actualizar estudiante:', error.message);
     }
 };
 
 const remove = async (rut) => {
     try {
-        const querySQL = 'DELETE FROM estudiantes WHERE rut = $1 RETURNING *';
-        await pool.query(querySQL, [rut]);
-        return { message: 'Estudiante eliminado correctamente' };
+        const querySQL = 'DELETE FROM estudiantes WHERE rut = $1';
+        const result = await pool.query({ text: querySQL, values: [rut] });
+        console.log('Estudiante eliminado correctamente:', result.rows[0]);
     } catch (error) {
-        // Manejar errores aquí si es necesario
-        throw error;
+        console.error('Error al eliminar estudiante:', error.message);
     }
 };
 
